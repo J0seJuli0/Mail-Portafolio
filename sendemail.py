5e7650e0eeaa95c7a59945b46ddff0db9d8ba9bc
J0seJuli0 <josejuliosanchezcruzado1@gmail.com>
2025-10-20 21:07:06 -0500

    update structure

--- sendemail.py
+++ sendemail.py
@@ -3,16 +3,8 @@ import json
 
 class handler(BaseHTTPRequestHandler):
     def do_POST(self):
-        content_length = int(self.headers.get('Content-Length', 0))
-        post_data = self.rfile.read(content_length)
-        data = json.loads(post_data.decode('utf-8'))
-
-        response = {
-            "message": "Correo recibido correctamente",
-            "data": data
-        }
-
         self.send_response(200)
-        self.send_header("Content-Type", "application/json")
+        self.send_header("Content-type", "application/json")
         self.end_headers()
-        self.wfile.write(json.dumps(response).encode())
+        response = {"message": "Email enviado correctamente"}
+        self.wfile.write(json.dumps(response).encode("utf-8"))
